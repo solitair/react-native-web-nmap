af8b139ef288514407356080b72ccee06e0af4b5
solitair <solitair@hitel.net>
2023-01-13 13:45:25 -0700

    remove react-native-maps.

--- examples/App.js
+++ examples/App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import MapView from 'react-native-maps';
 import NaverMapView, { Marker } from 'react-native-nmap';
 
 export default function App() {
--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -6,7 +6,6 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   // If you want to add a new alias to the config.
-  config.resolve.alias['react-native-maps'] = 'react-native-web-maps';
   config.resolve.alias['react-native-nmap'] = 'react-native-web-nmap';
   config.resolve.alias['react'] = path.join(__dirname, './node_modules/react');
   config.resolve.alias['react-dom'] = path.join(__dirname, './node_modules/react-dom');
